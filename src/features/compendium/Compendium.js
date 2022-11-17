88b17c5d5948cc41f1a0784308a112704aa3832c
Benoît Dinocourt <ghrind@gmail.com>
2022-11-17 18:53:48 +0100

    User can now filter compendium by alliances and factions

--- src/features/compendium/Compendium.js
+++ src/features/compendium/Compendium.js
@@ -1,29 +1,51 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { addWarrior } from '../roster/rosterSlice'
-import { List, Button } from 'semantic-ui-react'
+import { setAlliance, setFaction } from './compendiumSlice'
+import { Container, List, Button } from 'semantic-ui-react'
 
 export function Compendium() {
-  const warriors = useSelector( (state) => state.compendium.warriors );
+  const alliances = useSelector( (state) => state.compendium.alliances );
+  const alliance = useSelector( (state) => state.compendium.alliance );
+  const factions = useSelector( (state) => state.compendium.factions.filter(faction => faction.alliance === alliance) );
+  const faction = useSelector( (state) => state.compendium.faction );
+  const warriors = useSelector( (state) => state.compendium.warriors.filter(warrior => warrior.faction === faction) );
   const dispatch = useDispatch();
   return (
-    <List divided verticalAlign="middle">
-      {warriors && warriors.map( w =>
-        <List.Item>
-          <List.Header>
-            {w.name}
-          </List.Header>
-          <List.Content floated="right">
-            <Button onClick={ () => dispatch(addWarrior(w)) } aria-label="Add">+{w.cost}pts</Button>
-          </List.Content>
-          <List.Content>
-            <List celled horizontal>
-              <List.Item>S: 3</List.Item>
-              <List.Item>HP: 12</List.Item>
-            </List>
-          </List.Content>
-        </List.Item>
-      )}
-    </List>
+    <Container>
+      <List celled horizontal>
+        {alliances && alliances.map( a =>
+          <List.Item>
+            <Button onClick={() => dispatch(setAlliance(a))}>{a}</Button>
+          </List.Item>
+        )}
+      </List>
+      <List celled horizontal>
+        {factions && factions.map( f =>
+          <List.Item>
+            <Button onClick={() => dispatch(setFaction(f.name))}>{f.name}</Button>
+          </List.Item>
+        )}
+      </List>
+      <List divided verticalAlign="middle">
+        {warriors && warriors.map( w =>
+          <List.Item>
+            <List.Header>
+              {w.name}
+            </List.Header>
+            <List.Content floated="right">
+              <Button onClick={ () => dispatch(addWarrior(w)) } aria-label="Add">+{w.cost}pts</Button>
+            </List.Content>
+            <List.Content>
+              <List celled horizontal>
+                <List.Item>M: {w.move}</List.Item>
+                <List.Item>T: {w.toughness}3</List.Item>
+                <List.Item>HP: {w.hp}</List.Item>
+              </List>
+            </List.Content>
+          </List.Item>
+        )}
+      </List>
+    </Container>
   )
 }
 
--- src/features/compendium/compendiumSlice.js
+++ src/features/compendium/compendiumSlice.js
@@ -1,15 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
+  alliance: "Order",
+  alliances: ["Order", "Destruction", "Death", "Chaos"],
+  factions: [
+    { name: "Slaves to Darkness", alliance: "Chaos" },
+    { name: "Blades of Khorne", alliance: "Chaos" },
+    { name: "Khainite Shadowstalkers", alliance: "Order" },
+  ],
   warriors: [
-    { name: 'Morgoth', cost: 45 },
-    { name: 'Volkmar', cost: 75 },
+    { faction: "Slaves to Darkness", name: 'Chaos Spawn', cost: 45, move: 4, toughness: 3, hp: 12 },
+    { faction: "Slaves to Darkness", name: 'Exalted Champion of Chaos', cost: 75, move: 6, toughness: 4, hp: 20 },
+    { faction: "Blades of Khorne", name: 'Bloodreaper', cost: 75, move: 6, toughness: 4, hp: 20 },
+    { faction: "Khainite Shadowstalkers", name: 'Dark Elf', cost: 75, move: 6, toughness: 4, hp: 20 },
   ],
 };
 
 export const compendiumSlice = createSlice({
   name: 'compendium',
-  initialState
+  initialState,
+  reducers: {
+    setAlliance: (state, action) => {
+      state.alliance = action.payload;
+    },
+    setFaction: (state, action) => {
+      state.faction = action.payload;
+    },
+  }
 });
+
+export const { setAlliance, setFaction } = compendiumSlice.actions;
 
 export default compendiumSlice.reducer;
