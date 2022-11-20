8d0e8240824f2fb0a087d9381cb465ee8277ccc7
Benoît Dinocourt <ghrind@gmail.com>
2022-11-20 20:30:12 +0100

    Remove commented code

--- src/features/compendium/compendiumSlice.js
+++ src/features/compendium/compendiumSlice.js
@@ -4,18 +4,7 @@ import { getCompendiumData } from './compendiumAPI';
 const initialState = {
   alliance: "",
   faction: "",
-////alliances: [],
-//factions: [
-////{ name: "Slaves to Darkness", alliance: "Chaos" },
-////{ name: "Blades of Khorne", alliance: "Chaos" },
-////{ name: "Khainite Shadowstalkers", alliance: "Order" },
-//],
-  warriors: [
-  //{ faction: "Slaves to Darkness", name: 'Chaos Spawn', cost: 45, move: 4, toughness: 3, hp: 12 },
-  //{ faction: "Slaves to Darkness", name: 'Exalted Champion of Chaos', cost: 75, move: 6, toughness: 4, hp: 20 },
-  //{ faction: "Blades of Khorne", name: 'Bloodreaper', cost: 75, move: 6, toughness: 4, hp: 20 },
-  //{ faction: "Khainite Shadowstalkers", name: 'Dark Elf', cost: 75, move: 6, toughness: 4, hp: 20 },
-  ],
+  warriors: [],
 };
 
 export const loadData = createAsyncThunk(
