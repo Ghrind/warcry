4172108c5b07787979a41b665eb542e2a13ca791
Benoît Dinocourt <ghrind@gmail.com>
2022-11-21 19:55:35 +0100

    Save state in browser's local storage

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import rosterReducer from '../features/roster/rosterSlice';
 import compendiumReducer from '../features/compendium/compendiumSlice';
 import runeReducer from '../features/rune/runeSlice'
 
-export const store = configureStore({
-  reducer: {
-    roster: rosterReducer,
-    compendium: compendiumReducer,
-    rune: runeReducer,
-  },
+const rootReducer = combineReducers({
+  roster: rosterReducer,
+  compendium: compendiumReducer,
+  rune: runeReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({reducer: persistedReducer})
+export const persistor = persistStore(store)
